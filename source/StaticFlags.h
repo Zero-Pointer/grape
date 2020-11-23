249f05b2ae627cb5fbf9acc71e63a0dc7c0e90bd
zpt <1633001347@qq.com>
2020-11-23 19:00:09 +0800

    start

---
+++ source/StaticFlags.h
@@ -0,0 +1,53 @@
+//
+// Created by zpt1813061 on 2020/11/19.
+//
+
+#ifndef LEXICALANALYSIS1_0_STATICFLAGS_H
+#define LEXICALANALYSIS1_0_STATICFLAGS_H
+
+enum {
+    LT,
+    GT,
+    EQ,
+    LE,
+    NE,
+    GE,
+    LLK,
+    RLK,
+    LBK,
+    RBK,
+    IF,
+    ELSE,
+    EQU,
+    SEM,
+
+    WHILE,
+    DO,
+    ID,
+    NUMBER,
+    RELOP,
+
+    NEWLINE,
+    ERRORCHAR,
+    ADD,
+    DEC,
+    MUL,
+    DIV,
+
+    FOR,
+    TYPE,
+
+
+    // Type check
+    TYPE_NOT_MATCH,
+    TYPE_NOT_SUPPORT,
+    TYPE_MATCH,
+};
+enum TYPE_ENUM{
+    // Type const
+    TYPE_INT,
+    TYPE_DOUBLE,
+    TYPE_FLOAT,
+    TYPE_STRING,
+};
+#endif //LEXICALANALYSIS1_0_STATICFLAGS_H
